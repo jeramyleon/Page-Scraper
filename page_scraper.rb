9cb23ffe5edb1082d21419771f68f170025e47c8
jeramyleon <jeramyleon@gmail.com>
2016-03-05 19:57:39 -0500

    changes

--- page_scraper.rb
+++ page_scraper.rb
@@ -10,9 +10,6 @@ class Scraper
 	reddit_data = self.class.get("http://www.reddit.com/r/Kanye/.json")
 	reddit_data["data"]["children"].each do |story|
 	  scraped_data.push(story["data"]["url"].to_s)
-      if story["data"].include? "preview"
-	    scraped_data.push(story["data"]["preview"]["images"]["source"]["url"].to_s)
-      end	  
 	end
 	
 	puts scraped_data
